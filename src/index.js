1254fc9fa2068f08625227d14752adb67e6b2bb5
Modupetemi2k5 <151980601+Modupetemi2k5@users.noreply.github.com>
2024-01-18 13:01:16 -0800

    updating my codes

--- src/index.js
+++ src/index.js
@@ -7,23 +7,22 @@ function search(event) {
 }
 
 function displayForecast() {
-  let forecast = document.querySelector("#forecast");
   let forecastHtml = "";
   let days = ["mon", "tue", "wed", "thu", "fri"];
 
   days.forEach(function (day) {
     forecastHtml =
       forecastHtml +
-      `<div class="weatherforecast-day">< div class = "row " >
+      `
             < div class = "col-2 " >
             < div class = "weather-forecast-date " > ${day} < /div>
              < img src = " http://shecodes-assets.s3.amazonaws.com/api/weather/icons/few-clouds-day.png "
-        class = "weatherapp-icon" / >
-            </div> <
-            div class = "weatherforecast-app-temperature " >
-            < span class = "max-temp " > 18 < /span> <span class="min-temp ">12</span >
-            </div> </div></div>`;
+        class = "weatherapp-icon"/>
+            < div class = "weatherforecast-app-temperature " >
+         '   < span class = "max-temp " > 18 < /span> <span class="min-temp ">12</span >
+            </div> </div>`;
   });
+  let forecast = document.querySelector("#forecast");
   forecast.innerHTML = forecastHtml;
 }
 displayForecast();
@@ -40,8 +39,7 @@ function formatDate() {
   if (minute < 0) {
     minute = `0${minute}`;
   }
-  currentDate = `${day}${hour}${minute}`;
-
+  currentDate = `${day}${hour}:${minute}`;
   let timeElement = document.querySelector("#time");
   timeElement.innerHTML = currentDate;
 }
